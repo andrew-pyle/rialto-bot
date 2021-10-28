78d18ede6df41550e686d94ff1b3f16df18a12eb
Andrew Pyle <andrew.pyle@icloud.com>
2021-10-28 11:08:23 -0500

    Use cheerio instead of deno_dom. I think the stringified wasm was crashing Deno Deploy.

--- src/scrapeRialtoWebsite.ts
+++ src/scrapeRialtoWebsite.ts
@@ -1,7 +1,8 @@
 import {
-  DOMParser,
-  Node,
-} from "https://deno.land/x/deno_dom@v0.1.14-alpha/deno-dom-wasm.ts";
+  cheerio,
+  TagElement,
+  TextElement,
+} from "https://deno.land/x/cheerio@1.0.4/mod.ts";
 import parse from "https://deno.land/x/date_fns@v2.22.1/parse/index.js";
 
 export const RIALTO_URL = new URL("https://searcyrialtotheater.com");
@@ -16,24 +17,29 @@ interface RialtoData {
 export async function fetchRialtoData(): Promise<RialtoData> {
   const res = await fetch(RIALTO_URL);
   const html = await res.text();
-  const doc = new DOMParser().parseFromString(html, "text/html");
+  const $ = cheerio.load(html);
 
-  if (!doc) {
+  if (!$) {
     throw new Error(`Could not parse HTTP response from ${RIALTO_URL}`);
   }
 
-  const imdbLink = doc.querySelector("td a")?.attributes.href;
-  const relativePosterLink = doc.querySelector("td a img")?.attributes.src;
+  const imdbLink = $("td a").attr("href");
+  const relativePosterLink = $("td a img").attr("src");
 
   if (!imdbLink || !relativePosterLink) {
     throw new Error(
-      "Could not parse the Rialto website to get the 'Now Playing' Movie at the Rialto.",
+      "Could not parse the Rialto website to get the 'Now Playing' Movie at the Rialto."
     );
   }
 
+  // Show Times are a giant <h3> with <br> separators inside. WHY??
+  // <h3>Thr 2021-10-28 6:30 pm<br>Fri 2021-10-29 6:30 pm</h3>
+  const showTimesH3Contents = ($("td h3")[1] as TagElement).childNodes ?? [];
+  const showTimesText = showTimesH3Contents
+    .filter(isTextElement) // remove dumb <br> separators
+    .map((element) => element.data ?? "");
   // Parses each date in the runtime's local timezone
-  const showTimesHtml = doc.querySelectorAll("td h3")[1].childNodes;
-  const showTimes = parseRialtoDateString([...showTimesHtml]);
+  const showTimes = parseRialtoDateString(showTimesText);
 
   //   console.log({ showTimes }); // Debug
 
@@ -50,36 +56,39 @@ export async function fetchRialtoData(): Promise<RialtoData> {
 
 // fetchRialtoData(); // Debug
 
-function parseRialtoDateString(html: Node[]): Date[] {
+function parseRialtoDateString(html: string[]): Date[] {
   const empiricalFormatString = "E yyyy-MM-dd h:mm a";
-  return html.reduce<
-    Date[]
-  >(
-    (acc, node) => {
-      try {
-        // Remove whitespace, `(`, and `)`
-        // Also transform weekday abbreviations
-        const potentiallyATime = node.nodeValue?.trim().replaceAll("Thr", "Thu")
-          .replaceAll(/\(|\)/g, "");
+  return html.reduce<Date[]>((acc, node) => {
+    try {
+      // Remove whitespace, `(`, and `)`
+      // Also transform weekday abbreviations
+      const potentiallyATime = node //.nodeValue
+        ?.trim()
+        .replaceAll("Thr", "Thu")
+        .replaceAll(/\(|\)/g, "");
 
-        // Parse date string
-        const date = parse(
-          potentiallyATime,
-          empiricalFormatString,
-          new Date(), // This fallback date shouldn't be necessary, but it is required
-          {},
-        );
-        // Skip Invalid Dates
-        if (isNaN(date.getTime())) {
-          return acc;
-        }
-        // Return Valid Dates
-        return [...acc, date];
-      } catch {
-        // Skip Errors too
+      // Parse date string
+      const date = parse(
+        potentiallyATime,
+        empiricalFormatString,
+        new Date(), // This fallback date shouldn't be necessary, but it is required
+        {}
+      );
+      // Skip Invalid Dates
+      if (isNaN(date.getTime())) {
         return acc;
       }
-    },
-    [],
-  );
+      // Return Valid Dates
+      return [...acc, date];
+    } catch {
+      // Skip Errors too
+      return acc;
+    }
+  }, []);
+}
+
+function isTextElement(
+  element: TagElement | TextElement
+): element is TagElement {
+  return element.type === "text";
 }
